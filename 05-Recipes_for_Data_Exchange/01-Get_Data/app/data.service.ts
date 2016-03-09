d4494724e7394ad5f7f370b2595cbdcafa63877d
Nikolas Poniros <nikolas.poniros@get-the-service.de>
2016-03-09 15:18:00 +0100

    Add recipe on how to use http.get

---
+++ 05-Recipes_for_Data_Exchange/01-Get_Data/app/data.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+class DataService {
+  http: Http;
+  url: string;
+  constructor(http: Http) {
+    this.http = http;
+    this.url = 'http://127.0.0.1:3000/data';
+  }
+
+  getData() {
+    const observable = this.http.get(this.url);
+    const anotherObservable = observable.map((response) => response.json().data);
+    return anotherObservable;
+  }
+}
+
+export default DataService;
