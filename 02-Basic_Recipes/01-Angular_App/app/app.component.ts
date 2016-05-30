5cf39e76f0347a4caae2921ff2ffe45d10a3ca1f
Nikolas Poniros <nikolas.poniros@get-the-service.de>
2016-05-30 17:55:28 +0200

    rewrite for angular-cli

--- 02-Basic_Recipes/01-Angular_App/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'my-app',
-  template: '<div>Hello World!</div>'
-})
-class MyApp {}
-
-export default MyApp;
---
+++ 02-Basic_Recipes/01-Angular_App/src/app/demo.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-app',
+  template: '<div>Hello World!</div>'
+})
+export class DemoAppComponent {}
