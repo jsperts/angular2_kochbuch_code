b67e2b6c45a02e0c16ca33731e85c2beec164efc
Nikolas Poniros <nikolas.poniros@get-the-service.de>
2016-05-31 10:37:13 +0200

    Rename to match recipe number. Adjust for angular-cli

--- 02-Basic_Recipes/02-Define_Component/app/second.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'my-component',
-  template: '<div>My Name is ...</div>'
-})
-class MyComponent {}
-
-export default MyComponent;
---
+++ 02-Basic_Recipes/03-Define_Component/src/app/second.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  template: '<div>My Name is ...</div>'
+})
+export class SecondComponent {}
