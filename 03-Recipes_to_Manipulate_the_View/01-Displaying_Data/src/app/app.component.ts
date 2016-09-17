e1fb16ddb8086d9f2c99b5a88f4ad406a33e34b5
Nikolas Poniros <nikolas.poniros@jsperts.de>
2016-09-17 17:25:49 +0200

    update to ng2 final and angular-cli beta 14

--- 03-Recipes_to_Manipulate_the_View/01-Displaying_Data/src/app/app.component.ts
+++ 03-Recipes_to_Manipulate_the_View/01-Displaying_Data/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'demo-app',
+  selector: 'app-root',
   template: `
     <div>Hello World!</div>
     <div>My name is {{name}}</div>
     <div>My last name is {{lastname}}</div>
   `
 })
-export class DemoAppComponent {
+export class AppComponent {
   name: string;
   lastname = 'Mustermann';
 
