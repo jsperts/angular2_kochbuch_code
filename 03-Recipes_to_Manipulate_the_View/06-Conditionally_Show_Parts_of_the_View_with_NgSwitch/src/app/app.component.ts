c22c180783315a9b655ea27a4f188c7c30c87381
Nikolas Poniros <nikolas.poniros@jsperts.de>
2016-09-17 17:45:10 +0200

    update to ng2 final and angular-cli beta 14

--- 03-Recipes_to_Manipulate_the_View/06-Conditionally_Show_Parts_of_the_View_with_NgSwitch/src/app/app.component.ts
+++ 03-Recipes_to_Manipulate_the_View/06-Conditionally_Show_Parts_of_the_View_with_NgSwitch/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'demo-app',
+  selector: 'app-root',
   template: `
     <button (click)="showBlue()">Show blue</button>
     <button (click)="showRed()">Show red</button>
@@ -9,21 +9,21 @@ import { Component } from '@angular/core';
     <div>Variante 1</div>
     <div [ngSwitch]="color">
       <p>What color are you?</p>
-      <p *ngSwitchWhen="'blue'">I am blue</p>
-      <p *ngSwitchWhen="'red'">I am red</p>
+      <p *ngSwitchCase="'blue'">I am blue</p>
+      <p *ngSwitchCase="'red'">I am red</p>
       <p *ngSwitchDefault>Color not known</p>
     </div>
 
     <div>Variante 2</div>
     <div [ngSwitch]="color">
       <p>What color are you?</p>
-      <template [ngSwitchWhen]="'blue'"><p>I am blue</p></template>
-      <template [ngSwitchWhen]="'red'"><p>I am red</p></template>
+      <template [ngSwitchCase]="'blue'"><p>I am blue</p></template>
+      <template [ngSwitchCase]="'red'"><p>I am red</p></template>
       <template ngSwitchDefault><p>Color not known</p></template>
     </div>
   `
 })
-export class DemoAppComponent {
+export class AppComponent {
   color: string;
 
   constructor() {
