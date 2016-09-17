5989bd6dd7a7e631c54b68b17053ff8ab82c26fa
Nikolas Poniros <nikolas.poniros@jsperts.de>
2016-09-17 17:50:39 +0200

    update to ng2 final and angular-cli beta 14

--- 03-Recipes_to_Manipulate_the_View/07-Dynamic_Styles/Solution-01/src/app/app.component.ts
+++ 03-Recipes_to_Manipulate_the_View/07-Dynamic_Styles/Solution-01/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'demo-app',
+  selector: 'app-root',
   template: `
     <button type="button" (click)="showSmall()">Small</button>
     <button type="button" (click)="showBig()">Big</button>
     <div [ngStyle]="{'width': elemWidth, 'height': elemHeight}" style="background-color: red"></div>
   `
 })
-export class DemoAppComponent {
+export class AppComponent {
   elemWidth: string;
   elemHeight: string;
   constructor() {
