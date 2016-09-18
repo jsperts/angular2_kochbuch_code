841b1caebb82471de91ab816fab2a79b5fe2d254
Nikolas Poniros <nikolas.poniros@jsperts.de>
2016-09-18 14:05:41 +0200

    update to ng2 final and angular-cli beta 14

--- 04-Form_Recipes/02-Check_Form_Validity/Solution-02/src/app/app.component.ts
+++ 04-Form_Recipes/02-Check_Form_Validity/Solution-02/src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'demo-app',
+  selector: 'app-root',
   template: `
     <form (ngSubmit)="onSubmit(form)" #form="ngForm" novalidate>
       <label>Username</label>
-      <input type="text" [(ngModel)]="user.username" required ngControl="username"/>
+      <input type="text" [(ngModel)]="user.username" required name="username"/>
       <label>Password</label>
       <input type="password" [(ngModel)]="user.password"
-          required minlength="10" ngControl="password"/>
+          required minlength="10" name="password"/>
       <button type="submit">Submit</button>
     </form>
   `
 })
-export class DemoAppComponent {
+export class AppComponent {
   user = {
     username: '',
     password: ''
