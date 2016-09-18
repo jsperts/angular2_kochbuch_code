0d1797e6149253df77f9aeec3d4d8a61a3aa0b66
Nikolas Poniros <nikolas.poniros@jsperts.de>
2016-09-18 14:09:38 +0200

    update to ng2 final and angular-cli beta 14

--- 04-Form_Recipes/03-Form_Show_Error_for_Field/Solution-01/src/app/app.component.ts
+++ 04-Form_Recipes/03-Form_Show_Error_for_Field/Solution-01/src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'demo-app',
+  selector: 'app-root',
   template: `
     <form (ngSubmit)="onSubmit()" #form="ngForm" novalidate>
       <label>Username</label>
-      <input type="text" [(ngModel)]="user.username" required ngControl="username"/>
+      <input type="text" [(ngModel)]="user.username" required name="username"/>
       <div *ngIf="!form.controls.username?.valid">
         This field is required!
       </div>
       <label>Password</label>
       <input type="password" [(ngModel)]="user.password"
-          required minlength="10" ngControl="password"/>
+          required minlength="10" name="password"/>
       <div *ngIf="form.controls.password?.errors?.required">
         This field is required!
       </div>
@@ -22,7 +22,7 @@ import { Component } from '@angular/core';
     </form>
   `
 })
-export class DemoAppComponent {
+export class AppComponent {
   user = {
     username: '',
     password: ''
