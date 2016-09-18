6a0962f7a58190f7a7f589c4f2b5ba5e3787a6ce
Nikolas Poniros <nikolas.poniros@jsperts.de>
2016-09-18 13:30:37 +0200

    update to ng2 final and angular-cli beta 14

--- 07-Component_Recipes/05-Pass_Data_to_Parent_with_Outputs/src/app/app.component.ts
+++ 07-Component_Recipes/05-Pass_Data_to_Parent_with_Outputs/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { SecondComponent } from './second.component';
 
 @Component({
-  selector: 'demo-app',
+  selector: 'app-root',
   template: `
     <h1>Parent</h1>
     <p>Parent Data: {{parentData}}</p>
     <app-second (dataChange)="onDataChange($event)"></app-second>
-  `,
-  directives: [SecondComponent]
+  `
 })
-export class DemoAppComponent {
+export class AppComponent {
   parentData: string = 'Initial Data';
 
   onDataChange(data) {
