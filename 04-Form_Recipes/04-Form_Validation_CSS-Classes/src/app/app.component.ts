8579750d0e920132dc12f51a0771bf09d739b281
Nikolas Poniros <nikolas.poniros@jsperts.de>
2016-09-18 14:14:06 +0200

    update to ng2 final and angular-cli beta 14

--- 04-Form_Recipes/04-Form_Validation_CSS-Classes/src/app/app.component.ts
+++ 04-Form_Recipes/04-Form_Validation_CSS-Classes/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'demo-app',
+  selector: 'app-root',
   styles: [
     '.ng-invalid { border-color: red; }',
     '.ng-valid { border-color: green; }'
@@ -9,14 +9,14 @@ import { Component } from '@angular/core';
   template: `
     <form (ngSubmit)="onSubmit()" #form="ngForm" novalidate>
       <label>Username</label>
-      <input type="text" [(ngModel)]="user.username" required ngControl="username"/>
+      <input type="text" [(ngModel)]="user.username" required name="username"/>
       <label>Password</label>
       <input type="password" [(ngModel)]="user.password"
-          required minlength="10" ngControl="password"/>
+          required minlength="10" name="password"/>
       <button type="submit" [disabled]="!form.valid">Submit</button>
     </form>`
 })
-export class DemoAppComponent {
+export class AppComponent {
   user = {
     username: '',
     password: ''
